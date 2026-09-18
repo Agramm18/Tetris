a06402acf56e7b93ac058ab80e32c9108d96ee7c
Agramm18 <grammaleks@gmail.com>
2026-09-18 11:15:06 +0200

    Updated to recent structure

--- game/main_game/Screen_Modi/Play/play_window.h
+++ game/main_game/Screen_Modi/Play/play_window.h
@@ -1,15 +1,23 @@
 #ifndef PLAY_WINDOW_H
 #define PLAY_WINDOW_H
 
+#define FIELD_HEIGHT 20
+#define FIELD_WIDTH 10
 typedef enum {
-    START,
+    LOADING_GAME_SCREEN,
+    START_GAME,
     PLAYING,
     PAUSE,
     END
-} board_play_state;
+} PlayState;
+typedef struct playing_field {
+    int width;
+    int height;
+    int cells[FIELD_HEIGHT][FIELD_WIDTH];
+} playing_field;
 
 void drawStartScreen(Rectangle board, Rectangle start);
-void board_logic(board_play_state *state);
-void play_window(const int, const int);
+void board_logic(PlayState *state);
+void play_window(const int width,const int height, PlayState *state, playing_field *playingField);
 
 #endif
